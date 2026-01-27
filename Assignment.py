166f6895ab239f4279dfe97db61a3449e7e7c496
SNEHAA <252681754+snehaa-solanki@users.noreply.github.com>
2026-01-27 03:24:07 +0000

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,83 @@
+# print("hello snehaa(*_*)")
+"""a=10
+b=15
+print(a+b)"""
+
+"""num = 45
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")    
+
+year =2024
+if (year % 4== 0 and year %100 !=0):
+    print("leap year")
+else:
+    print("Not a leap year")    
+
+import math
+print(f"Standard PI : {math.pi}")    
+
+PI = 3.14
+GRAVITY = 9.8
+print(PI)
+print(GRAVITY)"""
+
+"""number = 8
+square = number * number
+print(square)
+
+num = int(input("Enter a number to square:"))
+square = num * num
+print("The square is :",square)"""
+
+""" PI = 3.14
+radius = 5
+area = PI *(radius ** 2)
+print("The area of the circle is :",area)"""
+"""
+# 1. Storing different types of data
+name = "Gemini"         
+age = 25                 
+price = 19.99           
+is_active = True        
+
+# 2. Checking and printing the data types
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(is_active))"""
+
+"""
+import math
+
+# 1. Square Root
+print(math.sqrt(64))     
+
+# 2. Rounding Up and Down
+print(math.ceil(4.2))     # Result: 5 (Ceiling - rounds UP)
+print(math.floor(4.8))    # Result: 4 (Floor - rounds DOWN)
+
+# 3. Constants
+print(math.pi)         
+
+# 4. Power (same as ** operator)
+print(math.pow(2, 3))     # Result: 8.0 (2 to the power of 3)"""
+
+"""base = 2
+exponent = 3
+
+result = base ** exponent
+print(result)  """
+
+
+# Ask the user for a number
+number = float(input("Enter a number: "))
+
+# Check the conditions
+if number > 0:
+    print("This is a Positive number.")
+elif number < 0:
+    print("This is a Negative number.")
+else:
+    print("The number is Zero.")
