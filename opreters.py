848fdef27a76c0c4c68dd1635ff2cee0b89daf78
SNEHAA <252681754+snehaa-solanki@users.noreply.github.com>
2026-01-27 03:47:09 +0000

    Add files via upload

---
+++ opreters.py
@@ -0,0 +1,49 @@
+#1 Arithmetic opreters
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+
+#2 assignment opreters
+#object-0
+x=10
+print(x)
+
+#3 unary minus
+a=5
+print(-a)
+
+#4 logical opreters
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#5 Boolean
+is_pass =True
+print(is_pass)
+
+#6 bitusise opreters
+a=5
+b=3
+print(a&b)
+print(a|b)
+
+#7 membership opreters
+number=[1,2,3,4,5]
+print(2 in number)
+print(6 not in number)
+
+#8 identify operters
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+#9 relational opereters
+p=10
+q=5
+print(p>q)
+print(p==q)
