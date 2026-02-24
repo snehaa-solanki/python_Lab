03d3d7f1841bc1cd17150c02789a8644f7c7e2d7
SNEHAA <252681754+snehaa-solanki@users.noreply.github.com>
2026-02-24 03:11:19 +0000

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,16 @@
+"""i=1 
+while i <= 10:
+    print(i)
+    i = i + 1"""
+"""n= int (input("Enter n :"))
+i=1
+s=0
+while i<=n :
+        s=s+i
+        i=i+1
+print ("sum =",s)"""
+num = int (input("Enter num : "))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1
