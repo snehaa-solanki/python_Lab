89595debbb9337fad5539c8355729ab43868be28
SNEHAA <252681754+snehaa-solanki@users.noreply.github.com>
2026-02-24 02:52:38 +0000

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,24 @@
+"""for i in range (1,6):
+    print (i)
+for i in range (10):
+        print ("Hello !")"""
+"""for i in range (1,11):
+      print (i)
+for i in range (1,21):
+      if i % 2 == 0:
+            print(i)"""      
+"""for i in range (1,16):
+      if i % 2 != 0:
+        print (i)"""
+"""for i in range (1,11):
+    print("5 X",i,"=",5*i)"""
+"""name = "Snehaa" 
+for letter in name :
+    print (letter)"""
+"""total = 0 
+for i in range (1,6):
+ total = total + i 
+ print("sum of total",total)"""
+numbers = [10,20,30,40]
+for n in numbers :
+    print(n)
