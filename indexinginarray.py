df97fe31773dc151a3c8f8d64a3a94d4cbff9421
SNEHAA <252681754+snehaa-solanki@users.noreply.github.com>
2026-03-07 09:01:46 +0530

    Add files via upload

---
+++ indexinginarray.py
@@ -0,0 +1,28 @@
+#basic slices
+'''import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])
+print(arr[:3])
+print(arr[2:])
+print(arr[:])'''
+
+#slicing with step
+'''arr=array('i',[10,20,30,40,50,60,70,80])
+print(arr[::2])
+print(arr[1::2])
+print(arr[::3])'''
+
+# negative slicing
+'''arr=array('i',[10,20,30,40,50])
+print(arr[-4:-1])
+print(arr[-3:])
+print(arr[:-2])'''
+
+#reverse array using slicing
+'''arr=array('i',[10,20,30,40,50])
+print(arr[::-1])'''
+
+#modifying slices
+'''arr=array('i',[10,20,30,40,50])
+arr[1:4]=array('i',[25,35,45])
+print(arr)'''
